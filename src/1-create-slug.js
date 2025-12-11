920138930bf63a5507d961dc5b2173cd618de312
Syed <xayanmay@gmail.com>
2025-12-11 14:17:14 -0500

    Completed

--- src/1-create-slug.js
+++ src/1-create-slug.js
@@ -1,6 +1,11 @@
 // Return a URL-friendly "slug": lowercase with hyphens instead of spaces.
 // Return null if the title contains banned characters: "!", "#", "?"
 const createSlug = (title) => {
+  if (title.includes('!') || title.includes('#') || title.includes('?')){
+    return null
+  } 
+
+  return title.toLowerCase().split(' ').join('-')
 
 };
 
--- src/2-user-profile.js
+++ src/2-user-profile.js
@@ -1,26 +1,36 @@
 // Return a new user object with username, email, isActive: true, loginCount: 0
 const createUser = (username, email) => {
-
+  return {
+    username: username,
+    email: email,
+    isActive: true,
+    loginCount: 0,
+  };
 };
 
 // Increase the user's loginCount by 1
 const incrementLogin = (user) => {
-
+  return (user.loginCount += 1);
 };
 
 // Set isActive to false and delete the email property. Return the user.
 const deactivateUser = (user) => {
-
+  user.isActive = false;
+  delete user.email;
+  return user;
 };
 
 // Print each property and value in the format "key: value"
 const printUserInfo = (user) => {
-
+  console.log(`username: ${user.username}`);
+  console.log(`email: ${user.email}`);
+  console.log(`isActive: ${user.isActive}`);
+  console.log(`loginCount: ${user.loginCount}`);
 };
 
 // BONUS: Return a true copy of the user object (not a reference)
 const cloneUser = (user) => {
-
+  return {...user}
 };
 
 module.exports = {
